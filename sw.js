e853d68a08e45865904aadb8f0f74160192fd86c
crazydb911 <crazydb911@gmail.com>
2026-05-11 23:20:26 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,33 @@
+const cacheName = "paizhuo-pwa-demo-v1";
+const assets = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./assets/icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(assets))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== cacheName).map((key) => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).catch(() => caches.match("./index.html"));
+    })
+  );
+});
